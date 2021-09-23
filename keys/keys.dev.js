a8c01730fc8d315b9b73e5d91111385dcfd6b0b6
Lukmanov87 <Lukmanovim87@gmail.com>
2021-09-23 20:20:26 +0300

    Second

---
+++ keys/keys.dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+   MONGODB_URI: `mongodb+srv://shopUser:Naxrtwq8dheexY61@cluster0.rart7.mongodb.net/shop`,
+   SESSION_SECRET: 'some secret value',
+   BASE_URL: 'http://localhost:3000'
+}
